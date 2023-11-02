03ce50bbbe4741c479f99776a0658f32774deb8d
norththinker <merabetsaib64@gmail.com>
2023-11-02 14:32:03 -0400

    Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        for (int i = 0; i < 8; i++) {
-            System.out.println(i);
-        }
+
+        System.out.println(1234);
     }
 }
